7ad88ee39cabb1254f3b2fa728ec3b3ef53abd2c
MargaritaAlvarezGonz <margarita.alvarez3105@gmail.com>
2023-10-16 12:28:38 -0600

    javascript

---
+++ src/app/dialog-b/ejercicios-javascript.js
@@ -0,0 +1,17 @@
+//operadores relacionales
+let a = 5
+let b = 6
+//operadores igualdad
+console.log("a == b -> ", a == b )
+
+//operador distinto
+
+console.log(`${a} > ${b} -> ${a > b} `)
+
+//Operadores logicos
+let ab = true
+let bc = false
+
+console.log (`${ab} && ${bc} -> ${ab && bc}` )
+
+
